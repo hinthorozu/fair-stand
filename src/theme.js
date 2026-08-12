ccda2f9daebc5cfb56a87a6068b7c12a6e7441df
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-12 03:15:19 +0300

    Use anodized aluminum profile color

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-export const ALUMINUM_PROFILE_COLOR = '#9AA0A6';
+export const ALUMINUM_PROFILE_COLOR = '#BFC3C5';
 
 if (typeof document !== 'undefined') {
   document.documentElement.style.setProperty('--aluminum-profile-color', ALUMINUM_PROFILE_COLOR);
