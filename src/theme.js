08436b59073ab4a1255395e239c79dd649adf3ce
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-12 03:11:16 +0300

    Add shared aluminum profile color

---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const ALUMINUM_PROFILE_COLOR = '#9AA0A6';
+
+if (typeof document !== 'undefined') {
+  document.documentElement.style.setProperty('--aluminum-profile-color', ALUMINUM_PROFILE_COLOR);
+}
