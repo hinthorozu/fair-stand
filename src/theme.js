a6670e6977194cfd92b49543e9fd497a694769dd
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-12 03:18:35 +0300

    Lighten anodized aluminum profile color

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-export const ALUMINUM_PROFILE_COLOR = '#BFC3C5';
+export const ALUMINUM_PROFILE_COLOR = '#D0D3D4';
 
 if (typeof document !== 'undefined') {
   document.documentElement.style.setProperty('--aluminum-profile-color', ALUMINUM_PROFILE_COLOR);
