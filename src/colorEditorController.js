46681872ddb3ecd7162d7044ff9dc2a7b745dbe5
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-09-03 13:21:09 +0300

    Add color editor synchronization controller

---
+++ src/colorEditorController.js
@@ -0,0 +1,65 @@
+import { colorValuesFromHex, cmykToRgb, rgbToHex } from './colorUtils.js';
+import { normalizeCmykValues, readNumberGroup } from './colorEditorInputs.js';
+
+function writeRgbInputs(inputs, rgb) {
+  inputs.r.value = String(rgb.r);
+  inputs.g.value = String(rgb.g);
+  inputs.b.value = String(rgb.b);
+}
+
+function writeCmykInputs(inputs, cmyk) {
+  inputs.c.value = String(cmyk.c);
+  inputs.m.value = String(cmyk.m);
+  inputs.y.value = String(cmyk.y);
+  inputs.k.value = String(cmyk.k);
+}
+
+export function createColorEditorController({
+  colorInput,
+  colorHexInput,
+  colorRgbInputs,
+  colorCmykInputs,
+  onApply = () => {},
+}) {
+  function syncFromHex(hex, { apply = false } = {}) {
+    const values = colorValuesFromHex(hex);
+    if (!values) return false;
+
+    colorInput.value = values.hex;
+    colorHexInput.value = values.hex;
+    writeRgbInputs(colorRgbInputs, values.rgb);
+    writeCmykInputs(colorCmykInputs, values.cmyk);
+
+    if (apply) onApply();
+    return true;
+  }
+
+  function syncFromRgbInputs() {
+    const rgb = readNumberGroup(colorRgbInputs);
+    if (!rgb) return false;
+    return syncFromHex(rgbToHex(rgb.r, rgb.g, rgb.b), { apply: true });
+  }
+
+  function syncFromCmykInputs() {
+    const cmyk = readNumberGroup(colorCmykInputs);
+    if (!cmyk) return false;
+
+    const normalizedCmyk = normalizeCmykValues(cmyk);
+    writeCmykInputs(colorCmykInputs, normalizedCmyk);
+
+    const rgb = cmykToRgb(
+      normalizedCmyk.c,
+      normalizedCmyk.m,
+      normalizedCmyk.y,
+      normalizedCmyk.k,
+    );
+    const hex = rgbToHex(rgb.r, rgb.g, rgb.b);
+    colorInput.value = hex;
+    colorHexInput.value = hex;
+    writeRgbInputs(colorRgbInputs, rgb);
+    onApply();
+    return true;
+  }
+
+  return { syncFromHex, syncFromRgbInputs, syncFromCmykInputs };
+}
